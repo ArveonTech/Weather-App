6e8304b11629a00f0af4aa084d128812104eb565
Ahda-Rizqi <AhdaRizqi1199@gmail.com>
2025-05-27 10:22:28 +0700

    update

--- src/components/atoms/titleFooter.jsx
+++ src/components/atoms/titleFooter.jsx
@@ -1,3 +1,3 @@
 export default function TitleFooter() {
-  return <h2 className="text-center h-10 font-semibold text-2xl">Created By : AshviaX</h2>;
+  return <h2 className="text-center h-10 font-semibold text-2xl">Created By : Arveon</h2>;
 }
